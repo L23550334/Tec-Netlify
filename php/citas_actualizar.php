27ee7c6fa24254f727d3585cd562653c47c3b515
Chris5599 <christian.balderraman@gmail.com>
2025-12-02 22:28:22 -0600

    Update citas_actualizar.php

--- php/citas_actualizar.php
+++ php/citas_actualizar.php
@@ -1,8 +1,9 @@
 <?php
-// php/citas_actualizar.php - Actualizar estado de citas
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
 include 'conexion.php';
-session_start();
 
 // Leer datos del request
 $data = json_decode(file_get_contents('php://input'), true);
